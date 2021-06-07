71b5e5fde990f4b430d830c8df41d10cdc8c75db
Nivedita-PA <npal4661@gmail.com>
2021-06-07 20:46:52 +0530

    First name regex validation introduced

---
+++ src/main/java/UserRegistration/UserRegistration.java
@@ -0,0 +1,38 @@
+package UserRegistration;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+    static Scanner sc = new Scanner(System.in);
+
+    private static final String First_Name = "[A-Z][a-z]{2,}";
+
+    //method validation
+    public static boolean checkValidation(String input, String inputPattern)
+    {
+        Pattern p = Pattern.compile(inputPattern);
+        if (input == null) {
+            return false;
+        }
+        Matcher m = p.matcher(input);
+        return m.matches();
+    }
+
+    public static boolean isValidFirstName(String firstName)
+    {
+        return checkValidation(firstName, First_Name);
+    }
+
+
+    public static void main(String[] args) {
+
+        //First Name
+        System.out.println("Enter first name of user");
+        String userName = sc.next();
+        System.out.println(isValidFirstName(userName));
+        
+    }
+
+}
